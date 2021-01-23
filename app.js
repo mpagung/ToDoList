aa957b5a109b91da723489c84a5cc1f3a6c13117
mpagung <michaelputra.agung@gmail.com>
2021-01-23 21:48:36 +0800

    Used given css from AppBrewery and added public folder for express.static

--- app.js
+++ app.js
@@ -3,42 +3,37 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items=["Buy Food","Cook Food","Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'))
 
 app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay()
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday"
-      break;
-    case 1:
-      day = "Monday"
-      break;
-    case 2:
-      day = "Tuesday"
-      break;
-    case 3:
-      day = "Wednesday"
-      break;
-    case 4:
-      day = "Thursday"
-      break;
-    case 5:
-      day = "Friday"
-      break;
-    case 6:
-      day = "Saturday"
-      break;
-    default:
-      console.log("Error: current day is equal to: "+currentDay);
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month: "long"
   }
 
-  res.render('list', {
-    day: day
-  })
-});
+  // const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+  // const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
+  // const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+  var day=today.toLocaleDateString("en-US", options);
+  res.render('list', {day: day,newListItems:items})
+})
+
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000")
